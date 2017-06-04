00a212e7812f09e8ef47fd99ae373a09e736a545
yuris001 <yuris4login@gmail.com>
2017-06-04 23:46:59 +0900

    for cors bottle

--- index.py
+++ index.py
@@ -1,18 +1,60 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from bottle import route,run,template,error
+"""
+Example of setting up CORS with Bottle.py.
+"""
 
-@route('/')
-def index():
-    return template('hoge') # viewsディレクトリ配下にhoge.tpl hoge.htmlがあった場合はhoge.tplが優先される
+from bottle import Bottle, request, response, run
+app = Bottle()
 
-@route('/api/<id>')
-def api(id):
-    return template('hoge',id=id)
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
-@error(404)
-def error404(error):
-    return template('404')
+@app.route('/api/login', method=['OPTIONS', 'POST'])
+def examples2():
+    if request.method == 'OPTIONS':
+        return {}
+    else:
+        return {'examples': [{
+            'id': 1,
+            'name': 'Foo'},{
+            'id': 2,
+            'name': 'Bar'}
+        ]}
 
-run(host='0.0.0.0', port=80, debug=True, reloader=True)
+@app.route('/examples', method=['OPTIONS', 'GET'])
+def examples():
+    """
+    If you are using something like Spine.js you'll need to
+    handle requests for the OPTIONS method. I haven't found a
+    DRY way to handle this yet. I tried setting up a hook for before_request,
+    but was unsuccessful for now.
+    """
+    if request.method == 'OPTIONS':
+        return {}
+    else:
+        return {'examples': [{
+            'id': 1,
+            'name': 'Foo'},{
+            'id': 2,
+            'name': 'Bar'}
+        ]}
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("--host", dest="host", default="localhost",
+                      help="hostname or ip address", metavar="host")
+    parser.add_option("--port", dest="port", default=8080,
+                      help="port number", metavar="port")
+    (options, args) = parser.parse_args()
+    run(app, host=options.host, port=int(options.port))
